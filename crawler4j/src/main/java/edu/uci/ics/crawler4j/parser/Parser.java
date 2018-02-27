9524c43027aa163cadf2e2447f9ce6c5c3cf5776
jakob svenningsson <jaksve@kth.se>
2018-02-27 19:41:44 +0100

    added measuremetns points to 2 new functions

--- crawler4j/src/main/java/edu/uci/ics/crawler4j/parser/Parser.java
+++ crawler4j/src/main/java/edu/uci/ics/crawler4j/parser/Parser.java
@@ -17,11 +17,16 @@
 
 package edu.uci.ics.crawler4j.parser;
 
+import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -60,6 +65,8 @@ public class Parser extends Configurable {
         parseContext = new ParseContext();
         parseContext.set(HtmlMapper.class, AllTagMapper.class.newInstance());
     }
+    
+	static boolean[] coverage = new boolean[32];
 
     /**
      * Parses the content of the page given as argument The parsed data is saved in the 
@@ -74,49 +81,70 @@ public class Parser extends Configurable {
 
     public void parse(Page page, String contextURL)
         throws NotAllowedContentException, ParseException {
+    		coverage[0] = true;
         if (Util.hasBinaryContent(page.getContentType())) { // BINARY
+    			coverage[1] = true;
             BinaryParseData parseData = new BinaryParseData();
             if (config.isIncludeBinaryContentInCrawling()) {
+        			coverage[2] = true;
                 if (config.isProcessBinaryContentInCrawling()) {
+            			coverage[3] = true;
                     parseData.setBinaryContent(page.getContentData());
                 } else {
+            			coverage[4] = true;
                     parseData.setHtml("<html></html>");
                 }
                 page.setParseData(parseData);
                 if (parseData.getHtml() == null) {
+            			coverage[5] = true;
+            			Parser.createFile("parse_coverage", coverage);
                     throw new ParseException();
+                } else {
+            			coverage[6] = true;
                 }
                 parseData.setOutgoingUrls(Net.extractUrls(parseData.getHtml()));
             } else {
+        			coverage[7] = true;
                 throw new NotAllowedContentException();
             }
         } else if (Util.hasPlainTextContent(page.getContentType())) { // plain Text
+    			coverage[8] = true;
             try {
                 TextParseData parseData = new TextParseData();
                 if (page.getContentCharset() == null) {
+            			coverage[9] = true;
                     parseData.setTextContent(new String(page.getContentData()));
                 } else {
+            			coverage[10] = true;
                     parseData.setTextContent(
                         new String(page.getContentData(), page.getContentCharset()));
                 }
                 parseData.setOutgoingUrls(Net.extractUrls(parseData.getTextContent()));
                 page.setParseData(parseData);
             } catch (Exception e) {
+        			coverage[11] = true;
                 logger.error("{}, while parsing: {}", e.getMessage(), page.getWebURL().getURL());
+    				Parser.createFile("parse_coverage", coverage);
                 throw new ParseException();
             }
         } else { // isHTML
+    			coverage[12] = true;
             Metadata metadata = new Metadata();
             HtmlContentHandler contentHandler = new HtmlContentHandler();
             try (InputStream inputStream = new ByteArrayInputStream(page.getContentData())) {
                 htmlParser.parse(inputStream, contentHandler, metadata, parseContext);
             } catch (Exception e) {
+        			coverage[13] = true;
                 logger.error("{}, while parsing: {}", e.getMessage(), page.getWebURL().getURL());
+    				Parser.createFile("parse_coverage", coverage);
                 throw new ParseException();
             }
 
             if (page.getContentCharset() == null) {
+        			coverage[14] = true;
                 page.setContentCharset(metadata.get("Content-Encoding"));
+            } else {
+        			coverage[15] = true;
             }
 
             HtmlParseData parseData = new HtmlParseData();
@@ -131,25 +159,33 @@ public class Parser extends Configurable {
 
             String baseURL = contentHandler.getBaseUrl();
             if (baseURL != null) {
+        			coverage[16] = true;
                 contextURL = baseURL;
+            } else {
+        			coverage[17] = true;
             }
 
             int urlCount = 0;
             for (ExtractedUrlAnchorPair urlAnchorPair : contentHandler.getOutgoingUrls()) {
-
+        			coverage[18] = true;
                 String href = urlAnchorPair.getHref();
                 if ((href == null) || href.trim().isEmpty()) {
+            			coverage[21] = true;
                     continue;
+                } else {
+            			coverage[22] = true;
                 }
 
                 String hrefLoweredCase = href.trim().toLowerCase();
                 if (!hrefLoweredCase.contains("javascript:") &&
                     !hrefLoweredCase.contains("mailto:") && !hrefLoweredCase.contains("@")) {
+            			coverage[23] = true;
                     // Prefer page's content charset to encode href url
                     Charset hrefCharset = ((page.getContentCharset() == null) || page.getContentCharset().isEmpty()) ?
                                           StandardCharsets.UTF_8 : Charset.forName(page.getContentCharset());
                     String url = URLCanonicalizer.getCanonicalURL(href, contextURL, hrefCharset);
                     if (url != null) {
+                			coverage[24] = true;
                         WebURL webURL = new WebURL();
                         webURL.setURL(url);
                         webURL.setTag(urlAnchorPair.getTag());
@@ -158,25 +194,52 @@ public class Parser extends Configurable {
                         outgoingUrls.add(webURL);
                         urlCount++;
                         if (urlCount > config.getMaxOutgoingLinksToFollow()) {
+                    			coverage[25] = true;
                             break;
+                        } else {
+                    			coverage[26] = true;
                         }
+                    } else {
+                			coverage[27] = true;
                     }
+                } else {
+            			coverage[28] = true;
                 }
             }
             parseData.setOutgoingUrls(outgoingUrls);
 
             try {
                 if (page.getContentCharset() == null) {
+            			coverage[29] = true;
                     parseData.setHtml(new String(page.getContentData()));
                 } else {
+            			coverage[30] = true;
                     parseData.setHtml(new String(page.getContentData(), page.getContentCharset()));
                 }
 
                 page.setParseData(parseData);
             } catch (UnsupportedEncodingException e) {
+        			coverage[31] = true;
+        			Parser.createFile("parse_coverage", coverage);
                 logger.error("error parsing the html: " + page.getWebURL().getURL(), e);
                 throw new ParseException();
             }
         }
+		Parser.createFile("parse_coverage", coverage);
     }
+    
+	public static void createFile(String fileName, boolean[] coverage) {
+	    	StringBuilder contents = new StringBuilder().append(Arrays.toString(coverage));
+	    	int count = 0;
+	    	for (int i = 0 ; i < coverage.length ; i++) {
+	    		if(coverage[i]) {count++;}
+	    	}
+	    	float percentCovered = (float) count / coverage.length;
+	    	contents.append("\n" + percentCovered * 100 + "%");
+	    	try {
+	    		Writer output = new BufferedWriter(new FileWriter(new File("test_coverage/" + fileName)));
+	    	    output.write(contents.toString());
+	    		output.close();
+	    	} catch(Exception e) { e.printStackTrace(); }	
+	}
 }
--- crawler4j/src/main/java/edu/uci/ics/crawler4j/robotstxt/PathRule.java
+++ crawler4j/src/main/java/edu/uci/ics/crawler4j/robotstxt/PathRule.java
@@ -1,5 +1,10 @@
 package edu.uci.ics.crawler4j.robotstxt;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
@@ -23,7 +28,11 @@ public class PathRule {
      * @param pattern The pattern to convert
      * @return The compiled regexp pattern created from the robots.txt pattern
      */
+    
+	static boolean[] coverage = new boolean[20];
+
     public static Pattern robotsPatternToRegexp(String pattern) {
+    		coverage[0] = true;
         StringBuilder regexp = new StringBuilder();
         regexp.append('^');
         StringBuilder quoteBuf = new StringBuilder();
@@ -32,51 +41,71 @@ public class PathRule {
         // If the pattern is empty, match only completely empty entries, e.g., none as
         // there will always be a leading forward slash.
         if (pattern.isEmpty()) {
+    			coverage[1] = true;
+    			PathRule.createFile("robotsPattern_coverage", coverage);
             return Pattern.compile("^$");
+        } else {
+    			coverage[2] = true;
         }
 
         // Iterate over the characters
         for (int pos = 0; pos < pattern.length(); ++pos) {
+    			coverage[3] = true;
             char ch = pattern.charAt(pos);
 
             if (ch == '\\') {
+        			coverage[4] = true;
+        	
                 // Handle escaped * and $ characters
                 char nch = pos < pattern.length() - 1 ? pattern.charAt(pos + 1) : 0;
                 if (nch == '*' || ch == '$') {
+            			coverage[5] = true;
                     quoteBuf.append(nch);
                     ++pos; // We need to skip one character
                 } else {
+            			coverage[6] = true;
                     quoteBuf.append(ch);
                 }
             } else if (ch == '*') {
+        			coverage[7] = true;
                 // * indicates any sequence of one or more characters
                 if (quoteBuf.length() > 0) {
+            			coverage[8] = true;
                     // The quoted character buffer is not empty, so add them before adding
                     // the wildcard matcher
                     regexp.append("\\Q").append(quoteBuf).append("\\E");
                     quoteBuf = new StringBuilder();
+                } else {
+            			coverage[9] = true;
                 }
                 if (pos == pattern.length() - 1) {
+            			coverage[10] = true;
                     terminated = true;
                     // A terminating * may match 0 or more characters
                     regexp.append(".*");
                 } else {
+            			coverage[11] = true;
                     // A non-terminating * may match 1 or more characters
                     regexp.append(".+");
                 }
             } else if (ch == '$' && pos == pattern.length() - 1) {
+        			coverage[12] = true;
                 // A $ at the end of the pattern indicates that the path should end here in order
               // to match
                 // This explicitly disallows prefix matching
                 if (quoteBuf.length() > 0) {
+            			coverage[13] = true;
                     // The quoted character buffer is not empty, so add them before adding
                     // the end-of-line matcher
                     regexp.append("\\Q").append(quoteBuf).append("\\E");
                     quoteBuf = new StringBuilder();
+                } else {
+            			coverage[14] = true;
                 }
                 regexp.append(ch);
                 terminated = true;
             } else {
+        			coverage[15] = true;
                 // Add the character as-is to the buffer for quoted characters
                 quoteBuf.append(ch);
             }
@@ -84,16 +113,23 @@ public class PathRule {
 
         // Add quoted string buffer: enclosed between \Q and \E
         if (quoteBuf.length() > 0) {
+    			coverage[16] = true;
             regexp.append("\\Q").append(quoteBuf).append("\\E");
+        } else {
+    			coverage[17] = true;
         }
 
         // Add a wildcard pattern after the path to allow matches where this
         // pattern matches a prefix of the path.
         if (!terminated) {
+    			coverage[18] = true;
             regexp.append(".*");
+        } else {
+    			coverage[19] = true;
         }
 
         // Return the compiled pattern
+		PathRule.createFile("robotsPattern_coverage", coverage);
         return Pattern.compile(regexp.toString());
     }
 
@@ -128,4 +164,19 @@ public class PathRule {
     public boolean matches(String path) {
         return this.pattern.matcher(path).matches();
     }
+    
+    public static void createFile(String fileName, boolean[] coverage) {
+	    	StringBuilder contents = new StringBuilder().append(Arrays.toString(coverage));
+	    	int count = 0;
+	    	for (int i = 0 ; i < coverage.length ; i++) {
+	    		if(coverage[i]) {count++;}
+	    	}
+	    	float percentCovered = (float) count / coverage.length;
+	    	contents.append("\n" + percentCovered * 100 + "%");
+	    	try {
+	    		Writer output = new BufferedWriter(new FileWriter(new File("test_coverage/" + fileName)));
+	    	    output.write(contents.toString());
+	    		output.close();
+	    	} catch(Exception e) { e.printStackTrace(); }	
+	}
 }
